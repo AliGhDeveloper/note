f1cc6130b1e8020a475fe4b2b1d93b572c3f8335
Ali gh <ghasempour.2m@gmail.com>
2022-10-02 12:14:56 +0330

    fixing errors

--- components/startup.js
+++ components/startup.js
@@ -1,14 +1,5 @@
-import { useEffect, useContext } from "react"
-import { getData } from "utiles/fetchData"
-import { Context } from "store/globalstate"
+
 export default function StartUp({children}) {
-    const { dispatch } = useContext(Context);
-    useEffect(() => {
-        getData('/notes') 
-            .then( result => {
-                if(!result.error) return dispatch({ type: 'ADD_NOTES', payload: result})
-            })
-    }, [])
     return (
         <>
             {children}
