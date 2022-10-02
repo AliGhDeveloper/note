08c669d6ebac5c212147634f53ef4c94a4b3773d
Ali gh <ghasempour.2m@gmail.com>
2022-10-02 12:11:39 +0330

    fixing errors

--- utiles/db_connect.js
+++ utiles/db_connect.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const db_connect = () => {
     if(mongoose.connections[0].readyState) return console.log('already connected');
 
-    return mongoose.connect(process.env.DB_URL, () => {
+    mongoose.connect(process.env.DB_URL, () => {
         console.log('connected to db')
     })
 }
